740d5f016c2cdcddbaa986962e056272fe0db110
Thomas <thomas.ir.rasor@gmail.com>
2024-06-16 12:32:19 -0500

    +save button for mass html file browser

--- htmlbrowser/htmlbrowser.js
+++ htmlbrowser/htmlbrowser.js
@@ -1,3 +1,33 @@
+export class HtmlBrowserTile
+{
+	constructor(fileIndex)
+	{
+		this.fileInfo = HtmlBrowser.instance.files[fileIndex];
+		this.fileData = HtmlBrowser.instance.fileDatum[fileIndex];
+
+		this.e_tile = AddElement("div", "file-grid-tile", "", HtmlBrowser.instance.e_gridView);
+		this.e_tile.innerHTML = this.fileData;
+
+		this.e_btn_save = AddElement("a", "file-grid-tile-save", "archive", this.e_tile);
+		this.e_btn_save.download = this.fileInfo.name.replace('.html', '') + '.html';
+		var dataBlob = new Blob([this.fileData], { type: 'text/html' });
+		this.dataBlobURL = window.URL.createObjectURL(dataBlob);
+		this.e_btn_save.href = this.dataBlobURL;
+	}
+
+	Remove()
+	{
+		ReleaseObjectURL();
+		this.e_tile.remove();
+	}
+
+	ReleaseObjectURL()
+	{
+		window.URL.revokeObjectURL(this.dataBlobURL);
+	}
+}
+
+
 export class HtmlBrowser
 {
 	static instance = new HtmlBrowser();
@@ -65,18 +95,14 @@ export class HtmlBrowser
 	{
 		if (this.fileTiles.length > 0)
 		{
-			for (var ii = 0; ii < this.fileTiles.length; ii++)
-			{
-				if (this.fileTiles[ii]) this.fileTiles[ii].remove();
-			}
+			for (var ii = 0; ii < this.fileTiles.length; ii++) this.fileTiles[ii].Remove();
 		}
 
 		this.fileTiles = [];
 		for (var ii = 0; ii < this.files.length; ii++)
 		{
-			var e_file = AddElement("div", "file-grid-tile", "", this.e_gridView);
-			e_file.innerHTML = this.fileDatum[ii];
-			this.fileTiles.push(e_file);
+			var tile = new HtmlBrowserTile(ii);
+			this.fileTiles.push(tile);
 		}
 	}
 }
@@ -92,8 +118,7 @@ function AddElement(kind, className = "", innerText = "", parent = null)
 	return e;
 }
 
-
-function CreateFileSelectionScreen()
+function CreateFileSelectionInput()
 {
 	var e_control_files = document.createElement("div");
 	var e_input_files = document.createElement("input");
@@ -116,4 +141,4 @@ function RemoveFileSelectionScreen()
 	e_control_files = null;
 }
 
-(() => { CreateFileSelectionScreen(); })();
+(() => { CreateFileSelectionInput(); })();
